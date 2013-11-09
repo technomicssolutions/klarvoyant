7fdf4a4a57284badc7936b6daa0bbc901e13ef67
Geethu <geethu@geethu-Lenovo-G570.(none)>
2013-11-09 18:17:07 +0530

    Added js file

--- klarvoyant/web/static/js/core_values.js
+++ klarvoyant/web/static/js/core_values.js
@@ -1,5 +1,20 @@
 window.addEvent('domready', function(){
-	$(nephro).show();
-	$(fertility).hide();
-	$(news).hide();
-}
+	$$('.nephro_div').show();
+	$$('.fertility_div').hide();
+	$$('.news_div').hide();
+	$$('.fertility').addEvent('click', function(e){
+		$$('.nephro_div').hide();
+		$$('.fertility_div').show();
+		$$('.news_div').hide();
+	}); 
+	$$('.news').addEvent('click', function(e){
+		$$('.nephro_div').hide();
+		$$('.fertility_div').hide();
+		$$('.news_div').show();
+	}); 
+	$$('.nephro').addEvent('click', function(e){
+		$$('.nephro_div').show();
+		$$('.fertility_div').hide();
+		$$('.news_div').hide();
+	});
+});
